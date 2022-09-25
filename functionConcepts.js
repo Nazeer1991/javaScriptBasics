203ad2fc1b1ec16afe5b7c3e9eb2b5d302dc7976
nazeersathak <nazeersathak@gmail.com>
2022-09-25 12:22:17 +0530

    functions Basics

--- functionConcepts.js
+++ functionConcepts.js
@@ -1,15 +1,17 @@
+//*************Functions Concepts in JavaScript ************/
+
 //a(); // it works
 //b(); // thorws error coz its just a variable till it is hitting the line of declaration there only it comes to know as function
 
 //Function Statement
-function a() {
-  console.log("a called");
-}
+// function a() {
+//   console.log("a called");
+// }
 
 // Function Expression
-var b = function () {
-  console.log("b called");
-};
+// var b = function () {
+//   console.log("b called");
+// };
 
 // Function Declaration
 //Function statement and function delaration all are same
@@ -18,16 +20,16 @@ var b = function () {
 //anonymous functions are used when functions are used as values
 
 // Named Function Expression
-var c = function xyz() {
-  console.log(
-    "function expression with a named function is named function expression "
-  );
-  console.log(xyz);
-};
+// var c = function xyz() {
+//   console.log(
+//     "function expression with a named function is named function expression "
+//   );
+//   console.log(xyz);
+// };
 //xyz() calling the above function like this throws error
 // you can call the function xyz inside the same function
 
-c(); // this way xyz function can be executed
+//c(); // this way xyz function can be executed
 
 // Difference between Parameters and Arguments
 //function name(param1,param2){
@@ -37,5 +39,6 @@ c(); // this way xyz function can be executed
 //First Class Functions
 // Ability of assinging a function to a variable  and passing function as argument to another function and function returned from function
 //passing function inide a function and returning function from a function
+// Functions are first Class citizens
 
 // Arrow Functions
